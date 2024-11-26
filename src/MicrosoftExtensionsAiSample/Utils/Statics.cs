fb982a6e42f589806191c6cbd1b679e21b541236
Sebastian Jensen <apps@tsjdev-apps.de>
2024-11-26 21:08:07 +0100

    Add sample project.

---
+++ src/MicrosoftExtensionsAiSample/Utils/Statics.cs
@@ -0,0 +1,12 @@
+﻿namespace MicrosoftExtensionsAiSample.Utils;
+
+internal static class Statics
+{
+    public const string OllamaKey = "Ollama";
+    public const string OpenAiKey = "OpenAI";
+    public const string AzureOpenAiKey = "Azure OpenAI";
+
+    public const string TextEmbedding3LargeModelName = "text-embedding-3-large";
+    public const string TextEmbedding3SmallModelName = "text-embedding-3-small";
+    public const string TextEmbeddingAdaModelName = "text-embedding-ada-002";
+}
